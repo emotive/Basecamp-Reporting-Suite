5803f545cbe53a9f57ad28950377d3fd613b64c1
mbriney <matthew@emotivellc.com>
2010-07-15 09:55:31 -0400

    Version 1.0

---
+++ basecamp/thismonth.php
@@ -0,0 +1,77 @@
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+
+
+
+<?php 
+
+// show errors
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+	
+// create connection	
+require('config.inc');
+
+
+?>
+
+
+<h1>Hours This Month</h1>
+<?php
+
+$startdate = date('Y-m-d', mktime(0, 0, 0, date('m'), 1, date('Y')));
+$enddate = date('Y-m-d', mktime(0, 0, 0, date('m'), 31, date('Y')));
+
+echo "<h3>".$startdate." - ".$enddate."</h3>";
+
+// Declare company hours variable
+
+$companyhours = 0;
+$totalemployees = 0;
+
+
+// get all of the company employees
+$response = $bc->getPeople();
+
+
+    foreach($response['body']->person as $person)
+    
+    	if ($person->{'company-id'} == $company_id) {    
+      		
+			$totalhours = 0;
+
+			// Get last week hours
+			$response_hours = $bc->getTimeEntryReport($from=$startdate, $to=$enddate);
+			//print_r($response);
+
+			foreach($response_hours['body']->{'time-entry'} as $time) {
+	
+				if ((int)$time->{'person-id'} == (int)$person->{'id'}) {
+				
+					// total up the hours
+					$totalhours = $totalhours + (float)$time->{'hours'};
+	
+				}
+	
+			}
+
+			// print the total
+			echo "<div class='person'>";
+      		echo $person->{'first-name'}." ".$person->{'last-name'}."<br><span class='number'>".round($totalhours,2)."</span><br><span class='summary'>".round(($totalhours/($business_hours*20.91)*100),1)."% complete</span>";
+      		echo "</div>";
+      		
+      		// add up the company hours
+      		$companyhours = $companyhours + $totalhours;
+			$totalemployees = $totalemployees + 1;
+
+		}
+
+
+// Display company hours
+
+echo "<div class='company'>";
+echo "company total<br><span class='number'>".round($companyhours,2)."</span><br><span class='summary'>".round(($companyhours/($business_hours*20.91*$totalemployees)*100),1)."% complete</span>";
+echo "</div>";
+
+?>
